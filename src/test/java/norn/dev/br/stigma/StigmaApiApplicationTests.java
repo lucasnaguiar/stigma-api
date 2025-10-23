e6bec0aac6cbab1398117e202c370be1fab5e216
Lucas Aguiar <lucasbarbary@gmail.com>
2025-10-23 12:15:16 -0500

    create project

---
+++ src/test/java/norn/dev/br/stigma/StigmaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package norn.dev.br.stigma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StigmaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
